5b7e9e6cc7a4898d87e298b21b9e7a36884588ce
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2021-09-24 23:12:48 +0700

    Mengelola asset di aplikasi MVC

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About me</h1>
-<p>Halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+  <h1 class="mt-4">About me</h1>
+  <img src="<?= BASEURL; ?>/img/pp.jpg" alt="Sofyan Egi" width="200" class="rounded-circle shadow">
+  <p>Halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,11 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Jumbo helper text</p>
+    <hr class="my-2">
+    <p>More info</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+    </p>
+  </div>
+</div>
