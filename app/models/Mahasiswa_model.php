174bb5e517730b5bbabb833325ebec7c501f1fa3
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2021-10-01 16:03:05 +0700

    Menambahkan fitur ubah data + ajax

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -33,7 +33,7 @@ class Mahasiswa_model
     $this->db->bind('nama', $data['nama']);
     $this->db->bind('nrp', $data['nrp']);
     $this->db->bind('email', $data['email']);
-    $this->db->bind('jurusan', $data['nama']);
+    $this->db->bind('jurusan', $data['jurusan']);
 
     $this->db->execute();
 
@@ -45,6 +45,28 @@ class Mahasiswa_model
     $query = "DELETE FROM mahasiswa Where id = :id";
     $this->db->query($query);
     $this->db->bind('id', $id);
+    $this->db->execute();
+
+    return $this->db->rowCount();
+  }
+
+  public function ubahDataMahasiswa($data)
+  {
+    $query = "UPDATE mahasiswa SET
+              nama = :nama,
+              nrp = :nrp,
+              email = :email,
+              jurusan = :jurusan
+            WHERE id = :id
+              ";
+
+    $this->db->query($query);
+    $this->db->bind('nama', $data['nama']);
+    $this->db->bind('nrp', $data['nrp']);
+    $this->db->bind('email', $data['email']);
+    $this->db->bind('jurusan', $data['jurusan']);
+    $this->db->bind('id', $data['id']);
+
     $this->db->execute();
 
     return $this->db->rowCount();
